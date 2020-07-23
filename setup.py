f30f4974a08a6b2f6a1eeaf30a4d501cf909163a
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2020-07-23 20:22:59 +0200

    Version bump to v1.1.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='pre-commit-hooks',
     description='Some out-of-the-box hooks for pre-commit',
     url='https://github.com/Lucas-C/pre-commit-hooks',
-    version='1.1.8',
+    version='1.1.9',
 
     author='Lucas Cimon',
     author_email='lucas.cimon@gmail.com',
