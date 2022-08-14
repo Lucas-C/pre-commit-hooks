cef09549d2a5fd7811f6fded453e8eb2f58f0a03
daniela-winkler <31401135+daniela-winkler@users.noreply.github.com>
2022-08-14 18:16:36 +0200

    Use python-Levenshtein instead of  python-Levenshtein-wheels in setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     packages=find_packages('.'),
     install_requires=[
         'fuzzywuzzy',
-        'python-Levenshtein-wheels',
+        'python-Levenshtein',
     ],
     entry_points={
         'console_scripts': [
