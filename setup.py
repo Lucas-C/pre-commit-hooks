062bad2e000e8d8b356d3260167326d3f3864a30
DanielaRuecker <Daniela.Ruecker-extern@deutschebahn.com>
2022-08-16 15:12:37 +0200

    use Levenshtein instead of python-Levenshtein
    python-Levenshtein only works for Linux users. Levenshtein should work for Windows users as well.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     packages=find_packages('.'),
     install_requires=[
         'fuzzywuzzy',
-        'python-Levenshtein',
+        'Levenshtein',
     ],
     entry_points={
         'console_scripts': [
