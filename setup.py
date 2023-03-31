7cd78bcaf99d366b95c929a28a41bb63e84de186
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2023-03-31 10:21:32 +0200

    Adding new hook: chmod (#71)

--- setup.py
+++ setup.py
@@ -2,38 +2,36 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='pre-commit-hooks',
-    description='Some out-of-the-box hooks for pre-commit',
-    url='https://github.com/Lucas-C/pre-commit-hooks',
-    version='1.4.2',
-
-    author='Lucas Cimon',
-    author_email='lucas.cimon@gmail.com',
-
-    platforms='linux',
+    name="pre-commit-hooks",
+    description="Some out-of-the-box hooks for pre-commit",
+    url="https://github.com/Lucas-C/pre-commit-hooks",
+    version="1.4.2",
+    author="Lucas Cimon",
+    author_email="lucas.cimon@gmail.com",
+    platforms="linux",
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
-
-    packages=find_packages('.'),
+    packages=find_packages("."),
     install_requires=[
-        'fuzzywuzzy',
-        'python-Levenshtein',
+        "fuzzywuzzy",
+        "python-Levenshtein",
     ],
     entry_points={
-        'console_scripts': [
-            'forbid_crlf = pre_commit_hooks.forbid_crlf:main',
-            'forbid_tabs = pre_commit_hooks.forbid_tabs:main',
-            'insert_license = pre_commit_hooks.insert_license:main',
-            'remove_crlf = pre_commit_hooks.remove_crlf:main',
-            'remove_tabs = pre_commit_hooks.remove_tabs:main',
+        "console_scripts": [
+            "chmod = pre_commit_hooks.chmod:main",
+            "forbid_crlf = pre_commit_hooks.forbid_crlf:main",
+            "forbid_tabs = pre_commit_hooks.forbid_tabs:main",
+            "insert_license = pre_commit_hooks.insert_license:main",
+            "remove_crlf = pre_commit_hooks.remove_crlf:main",
+            "remove_tabs = pre_commit_hooks.remove_tabs:main",
         ],
     },
 )
--- tests/text_utils_test.py
+++ tests/text_utils_test.py
@@ -4,12 +4,12 @@ from pre_commit_hooks.utils import is_text
 
 
 @pytest.mark.parametrize(
-    ('input_s', 'expected'),
+    ("input_s", "expected"),
     (
-        (b'foo \r\nbar', True),
-        (b'<a>\xe9 \n</a>\n', False),
-        (b'foo \x00bar', False),  # NUL character is not considered text
-        (b'', True),              # Empty file is considered text
+        (b"foo \r\nbar", True),
+        (b"<a>\xe9 \n</a>\n", False),
+        (b"foo \x00bar", False),  # NUL character is not considered text
+        (b"", True),  # Empty file is considered text
     ),
 )
 def test_is_text(input_s, expected):
