dad83f94f8e519bd7ee6bb1dc669b5521cdd2d45
Max Bachmann <kontakt@maxbachmann.de>
2023-03-31 10:30:00 +0200

    replace fuzzywuzzy with rapidfuzz (#58)
    Co-authored-by: Lucas Cimon <925560+Lucas-C@users.noreply.github.com>

--- pre_commit_hooks/insert_license.py
+++ pre_commit_hooks/insert_license.py
@@ -6,7 +6,7 @@ import sys
 from datetime import datetime
 from typing import Any, Sequence
 
-from fuzzywuzzy import fuzz
+from rapidfuzz import fuzz
 
 FUZZY_MATCH_TODO_COMMENT = (
     " TODO: This license is not consistent with the license used in the project."
--- setup.py
+++ setup.py
@@ -21,8 +21,7 @@ setup(
     ],
     packages=find_packages("."),
     install_requires=[
-        "fuzzywuzzy",
-        "python-Levenshtein",
+        'rapidfuzz',
     ],
     entry_points={
         "console_scripts": [
