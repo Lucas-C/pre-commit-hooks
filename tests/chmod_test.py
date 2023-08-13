e4a7573b780614a1295d454147884190087bcfe5
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2023-08-13 16:17:15 +0200

    Disabling chmod hook under Windows

--- tests/chmod_test.py
+++ tests/chmod_test.py
@@ -1,13 +1,21 @@
+import sys
 from pre_commit_hooks.chmod import main as chmod
 
-from .utils import chdir_to_test_resources
+from .utils import chdir_to_test_resources, capture_stdout
 
 
 def test_chmod_ok():
     with chdir_to_test_resources():
-        assert chmod(["755", "module_with_license.py"]) == 1
-        assert chmod(["644", "module_with_license.py"]) == 1
-        assert chmod(["644", "module_with_license.py"]) == 0
+        if sys.platform == "win32":
+            with capture_stdout() as stdout:
+                assert chmod(["755", "module_with_license.py"]) == 0
+            assert (
+                "This hook does nothing when executed on Windows" in stdout.getvalue()
+            )
+        else:
+            assert chmod(["755", "module_with_license.py"]) == 1
+            assert chmod(["644", "module_with_license.py"]) == 1
+            assert chmod(["644", "module_with_license.py"]) == 0
 
 
 def test_invalid_perms():
